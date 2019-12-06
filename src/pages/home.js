4abc7d2d6adb4d0b5be530aca97aa8579f1122e1
jordancarlson <jordan.p.carlson@gmail.com>
2019-12-06 12:43:14 -0800

    added pages/ and redux/ directories. added code for home.js and login.js -- going to finish adding code to register.js and necessary redux stuff

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <header className="App-header"/>
+        </div>
+    );
+};
+
+export default Home;
